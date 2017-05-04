d2245bab20d979ac69557618dcf50f5039dbc5b9
Dezhang1999 <1162838174@qq.com>
2017-05-04 16:47:36 -0700

    Update guts.js

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -3,34 +3,56 @@
     // multiply, divide, subtract, add, power, square_root, logBase10, generateRandomNumber
 
 
+// 1. divide
+//    write a function named "divide" that takes in two parameters and returns their quotient
 function divide(x,y) {
-	//Write your code below this line
-
+	return x/y;
 }
 
+// 2. multiply
+// write a function named
+// "multiply" that takes in two
+// parameters and returns
+// their product
 function multiply(x,y) {
-
+    return x*y;
 }
 
+
+// 3. subtract 
+//    write a function named
+//    "subtract" that takes in two
+// parameters and returns
+// their difference
 function subtract(x,y) {
-
+    return x-y;
 }
 
-function add(x,y) {
 
+// 4. add
+//    write a function named "add" that takes in two parameters and returns their sum
+function add(x,y){
+    return x+y;
 }
 
-function power(x,y) {
-
+// 5. power
+//    write a function named "power" that takes in two parameters and returns their result
+function power(x,y){
+return Math.pow(x,y);
 }
 
-function square_root(x) {
-
+// 6. square_root
+//    write a function named "square_root" that takes in one parameter and returns their result
+function power(x){
+return Math.pow(x,0.5);
 }
 
-function logBase10(x) {
-
+// 7. logBase10
+//    write a function named "logBase10" that takes in one parameter and returns their result
+function logBase10 (x){
+    return Math.log10(x);
 }
+
 
 function generateRandomNumber() {
 
